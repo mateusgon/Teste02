7054f5ad68ba6e6e18cd26b6de1ddd29e78e0846
Mateus Gonçalo <mateusgon57@gmail.com>
2018-11-15 11:05:19 -0200

    Mudando

--- app/src/main/java/com/example/mateu/dcc196_trabalho02/VideoActivity.java
+++ app/src/main/java/com/example/mateu/dcc196_trabalho02/VideoActivity.java
@@ -1,7 +1,10 @@
 package com.example.mateu.dcc196_trabalho02;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.teste_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
